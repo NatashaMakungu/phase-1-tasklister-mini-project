c5bd03b05930e467650c804cbd556b7d6a9d1484
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-03-30 12:58:24 +0300

    complete src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  selectTag()
+  let form = document.querySelector('form')
 });
+
+function createNewTask(newTask, color) {
+  const li = document.createElement('li')
+  const button = document.createElement('button')
+  deleteTask(button)
+  button.textContent = 'X'
+  li.textContent = `${newTask}`
+  li.style = `color: ${color}`
+  document.querySelector('ul').appendChild(li).appendChild(button)
+}
+
+function deleteTask(element) {
+  element.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+}
+
+function handleTask(element) {
+  element.addEventListener('submit', (e) => {
+    e.parentDefault()
+    const newTask = e.target['new-task-description'].value
+    const select = e.target[1]
+    const color = select.options[select.selectedIndex].value
+    console.log(color);
+    createNewTask(newTask, color)
+    element.reset()
+  })
+}
+
+function selectTag() {
+  const select = document.getElementById('colors')
+  const colors = [' ', 'red', 'orange', 'green']
+  for(let color of colors) {
+    let option = document.createElement('option')
+    option.textContent = color
+    option.style = `backgroundColor: ${color}`
+    select.appendChild(option)
+  }
+  return select
+}
